e49eb05b378a073807d4cad9628b6b9191a74acd
Milan <kicamilan@gmail.com>
2020-02-04 23:43:31 +0100

    qa-Milan-Domaci

---
+++ src/com/comtrade/entry/EntryPoint.java
@@ -0,0 +1,41 @@
+package com.comtrade.entry;
+
+import java.util.*;
+
+public class EntryPoint {
+
+	public static void main(String[] args) {
+
+		List<Integer> firstList = new ArrayList<Integer>();
+		firstList.add(1);
+		firstList.add(2);
+		firstList.add(3);
+		firstList.add(5);
+		firstList.add(6);
+
+		List<Integer> secondList = new ArrayList<Integer>();
+		secondList.add(2);
+		secondList.add(3);
+		secondList.add(8);
+
+		List<Integer> thirdList = new ArrayList<Integer>();
+
+		for (int i = 0; i < firstList.size(); i++) {
+			for (int j = 0; j < secondList.size(); j++) {
+				if (firstList.get(i) == secondList.get(j)) {
+					thirdList.add(i);
+					System.out.println(firstList.get(i));
+				}
+			}
+		}
+	}
+}
+
+//		for (int i : firstList) {
+//			if (secondList.contains(i)) {
+//				thirdList.add(i);
+//			}
+//		}
+//		System.out.println(thirdList);
+//	}
+//}
